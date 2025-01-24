75f840bcf994ff8e6e9bbd29178f84cfc49d6e0e
harkirat <harkirat96@gmail.com>
2025-01-24 21:28:28 +0530

    Init repo

---
+++ indexer/index.ts
@@ -0,0 +1,11 @@
+
+let CURRENT_BLOCK_NUMBER = 123123123;
+
+async function main() {
+    // get the interested addresses from the DB
+
+    // Inspect the block for native eth transactions on one of these addresses
+
+    // Bad approach => Update the balance in the database. 
+
+}
